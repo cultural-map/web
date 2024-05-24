1d678e1c7d5c6099829daec520225ff89f4f6591
gabruw <gabrielmarques.mg@gmail.com>
2024-05-24 16:45:45 -0300

    feat: bump and removing some packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import { nextui } from "@nextui-org/theme";
-
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/theme";
 
 const config: Config = {
 	darkMode: "class",
