2896f4ec47345288ec1aada3a547f282dbeed2d9
gabruw <gabrielmarques.mg@gmail.com>
2024-09-19 17:31:31 -0300

    feat(translations): adding translations for next features

--- src/translations/pt-BR/index.ts
+++ src/translations/pt-BR/index.ts
@@ -1,7 +1,11 @@
 import common from "./common.json";
+import cultural_map from "./cultural_map.json";
 import fields from "./fields.json";
+import geolocation from "./geolocation.json";
 import home from "./home.json";
 import language from "./language.json";
+import profile from "./profile.json";
+import project from "./project.json";
 import sign_in from "./sign_in.json";
 import theme from "./theme.json";
 import zod from "./zod.json";
@@ -14,6 +18,10 @@ export const ptBR = {
 		common,
 		fields,
 		sign_in,
+		profile,
+		project,
 		language,
+		geolocation,
+		cultural_map,
 	},
 };
