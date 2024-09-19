b6c295efd2cd7dce6ff2c33b03f6d226bc7f5ea4
gabruw <gabrielmarques.mg@gmail.com>
2024-09-19 17:34:49 -0300

    chore(tailwind): adding font and rotate plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
 import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/theme";
 
+import { rotateAxios } from "./src/configurations/tailwind/rotate-axios";
+
 const config: Config = {
 	darkMode: "class",
-	plugins: [nextui()],
+	plugins: [nextui(), rotateAxios()],
 	content: ["./src/**/*.{ts,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			barlow: ['"Barlow"', "sans-serif"],
+		},
+	},
 };
 
 export default config;
